7daac2aa5acb31d628d88ddf8e5b2383d934808b
AjemarS <sashabratashchuksasha@gmail.com>
2025-12-04 11:54:21 +0200

    Added github as auth provider

--- auth-service/src/auth.ts
+++ auth-service/src/auth.ts
@@ -42,7 +42,12 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      redirectURI: `${process.env.BETTER_AUTH_URL || "http://localhost:3001/auth"}/callback/google`,
+      redirectURI: process.env.GOOGLE_REDIRECT_URL || "http://localhost:3001/auth/callback/google",
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+      redirectURI: process.env.GITHUB_REDIRECT_URL || "http://localhost:3001/auth/callback/github",
     },
   },
   
